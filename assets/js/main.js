a13ac7b9cdfb908fdee1f9db3af663f1bd3c23ac
felip-araujo <felipearaujoexecutive@gmail.com>
2024-03-19 16:01:08 -0400

    16h01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,68 @@
-setTimeout(function() {
-    document.querySelector('.ability').classList.add('highlighted');
-}, 1000);
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollDownIcon = document.querySelector('.scroll-down-icon');
+    const targetSection = document.querySelector('#about'); // Defina aqui a seção para onde deseja rolar
+
+    scrollDownIcon.addEventListener('click', function() {
+        smoothScroll(targetSection);
+    });
+
+    function smoothScroll(target) {
+        const targetPosition = target.getBoundingClientRect().top + window.pageYOffset;
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        const duration = 1500; // Duração da animação em milissegundos
+        let startTime = null;
+
+        function animation(currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const ease = easeInOutCubic(timeElapsed, startPosition, distance, duration);
+            window.scrollTo(0, ease);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        }
+
+        function easeInOutCubic(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t * t + b;
+            t -= 2;
+            return c / 2 * (t * t * t + 2) + b;
+        }
+
+        requestAnimationFrame(animation);
+    }
+}); 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollDownIcon = document.querySelector('.ability');
+    const targetSection = document.querySelector('#skills'); // Defina aqui a seção para onde deseja rolar
+
+    scrollDownIcon.addEventListener('click', function() {
+        smoothScroll(targetSection);
+    });
+
+    function smoothScroll(target) {
+        const targetPosition = target.getBoundingClientRect().top + window.pageYOffset;
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        const duration = 1500; // Duração da animação em milissegundos
+        let startTime = null;
+
+        function animation(currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const ease = easeInOutCubic(timeElapsed, startPosition, distance, duration);
+            window.scrollTo(0, ease);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        }
+
+        function easeInOutCubic(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t * t + b;
+            t -= 2;
+            return c / 2 * (t * t * t + 2) + b;
+        }
+
+        requestAnimationFrame(animation);
+    }
+});
+
