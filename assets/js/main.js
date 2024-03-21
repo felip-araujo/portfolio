a49098bb2a0bf97d6e5ef28d96cf979d36670332
felip-araujo <felipearaujoexecutive@gmail.com>
2024-03-21 08:30:30 -0400

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const scrollDownIcon = document.querySelector('.scroll-down-icon');
-    const targetSection = document.querySelector('#about'); // Defina aqui a seção para onde deseja rolar
+    const scrollDownIconAbout = document.querySelector('.scroll-down-icon');
+    const scrollDownIconSkills = document.querySelector('.ability');
+    const aboutSection = document.querySelector('#about');
+    const skillsSection = document.querySelector('#skills');
 
-    scrollDownIcon.addEventListener('click', function() {
-        smoothScroll(targetSection);
+    scrollDownIconAbout.addEventListener('click', function() {
+        smoothScroll(aboutSection);
     });
 
-    function smoothScroll(target) {
-        const targetPosition = target.getBoundingClientRect().top + window.pageYOffset;
-        const startPosition = window.pageYOffset;
-        const distance = targetPosition - startPosition;
-        const duration = 1500; // Duração da animação em milissegundos
-        let startTime = null;
-
-        function animation(currentTime) {
-            if (startTime === null) startTime = currentTime;
-            const timeElapsed = currentTime - startTime;
-            const ease = easeInOutCubic(timeElapsed, startPosition, distance, duration);
-            window.scrollTo(0, ease);
-            if (timeElapsed < duration) requestAnimationFrame(animation);
-        }
-
-        function easeInOutCubic(t, b, c, d) {
-            t /= d / 2;
-            if (t < 1) return c / 2 * t * t * t + b;
-            t -= 2;
-            return c / 2 * (t * t * t + 2) + b;
-        }
-
-        requestAnimationFrame(animation);
-    }
-}); 
-
-document.addEventListener("DOMContentLoaded", function() {
-    const scrollDownIcon = document.querySelector('.ability');
-    const targetSection = document.querySelector('#skills'); // Defina aqui a seção para onde deseja rolar
-
-    scrollDownIcon.addEventListener('click', function() {
-        smoothScroll(targetSection);
+    scrollDownIconSkills.addEventListener('click', function() {
+        smoothScroll(skillsSection);
     });
 
     function smoothScroll(target) {
@@ -65,4 +37,3 @@ document.addEventListener("DOMContentLoaded", function() {
         requestAnimationFrame(animation);
     }
 });
-
